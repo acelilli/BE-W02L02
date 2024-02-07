78436125b9f4e26c4f57cc00223ee2d25adcdabe
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-02-07 14:26:12 +0100

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -6,5 +6,6 @@ SELECT * FROM Employees WHERE City = 'London';
 --------------------------------------------
 SELECT * FROM Orders ORDER BY Freight DESC; -- ordine discendente
 SELECT * FROM Orders WHERE Freight > 90 AND Freight < 200; -- tra 90 e 200
-SELECT * FROM [Order Details] WHERE Discount > 0; --nb [Order Details] perché c'è lo spazio
 SELECT * FROM Orders WHERE CustomerID = 'BOTTM' AND Freight > 50; 
+---------------------------------------------------------------
+SELECT * FROM [Order Details] WHERE Discount > 0; --nb [Order Details] perché c'è lo spazio
